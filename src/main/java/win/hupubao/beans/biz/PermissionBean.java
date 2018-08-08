3aab63ce1d6b5c233114d31964e4abd77c3f2b18
ysdxz207 <ysdxz207@qq.com>
2018-08-08 23:41:23 +0800

    user

--- src/main/java/win/hupubao/beans/biz/PermissionBean.java
+++ src/main/java/win/hupubao/beans/biz/PermissionBean.java
@@ -3,36 +3,29 @@ package win.hupubao.beans.biz;
 import win.hupubao.domain.Permission;
 
 import javax.persistence.Table;
+import java.util.List;
 
 @Table(name = "permission")
 public class PermissionBean extends Permission {
 	private static final long serialVersionUID = 1L;
 
-	private String menuName;
-	private String pid;
-	private Boolean isChecked;
-
-	public String getMenuName() {
-		return menuName;
-	}
-
-	public void setMenuName(String menuName) {
-		this.menuName = menuName;
-	}
-
-	public String getPid() {
-		return pid;
-	}
-
-	public void setPid(String pid) {
-		this.pid = pid;
-	}
+	private Boolean checked;
+	private List<PermissionBean> children;
 
 	public Boolean getChecked() {
-		return isChecked;
+		return checked;
 	}
 
 	public void setChecked(Boolean checked) {
-		isChecked = checked;
+		this.checked = checked;
+	}
+
+
+	public List<PermissionBean> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<PermissionBean> children) {
+		this.children = children;
 	}
 }
